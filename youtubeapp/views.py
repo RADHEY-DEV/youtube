cd78b25aea7c93c54b42d706ea9530749e461df3
radhey.baps@gmail.com <radhey.dineshkumar.panchal@ibm.com>
2021-05-06 16:26:56 -0400

    first commit

---
+++ youtubeapp/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from pytube import YouTube
+from django.http import FileResponse
+
+import os
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def download(request):
+    global url
+    url = request.GET.get('url')
+    yt = YouTube(url)
+    video = []
+    video = yt.streams.filter(progressive=True).all()
+    embed_link = url.replace("watch?v=", "embed/")
+    Title = yt.title
+    context = {'video': video, 'embed': embed_link, 'title': Title}
+    return render(request, 'download.html', context)
+
+def yt_download_done(request, resolution):
+    global url
+    homedir = os.path.expanduser("~")
+    dirs = homedir + '/Downloads'
+    if request.method == "POST":
+        return FileResponse(open(YouTube(url).streams.get_by_resolution(resolution).download(skip_existing=True),'rb'))
+        return render(request, 'done.html')
+    else:
+        return render(request, 'error.html')
